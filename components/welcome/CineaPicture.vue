854c0fcac14e0a78d10445f72ca05a50f10128f3
Zhang Yao <cineazhan@gmail.com>
2024-01-07 20:39:19 +0800

    dev: 首页基本完成

--- components/welcome/CineaPicture.vue
+++ components/welcome/CineaPicture.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="relative">
-    <div class="relative z-[21]"><img src="img/home/D03.png" alt="" class="w-[520px] lg:w-[680px]" /></div>
+    <div class="relative z-[21]"><img src="/img/home/D03.png" alt="" class="w-[520px] lg:w-[680px]" /></div>
     <div class="absolute z-[20] top-[120px] -left-[100px] md:top-[110px] md:-left-[250px] lg:top-[132px] lg:-left-[300px]">
       <div class="font-extrabold font-['Urbanist'] text-8xl md:text-9xl lg:text-[120pt]">Zhang</div>
     </div>
